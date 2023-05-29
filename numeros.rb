998572598d42f9d2b864226aafbd4385ce07e11c
Laura <lauracolof@gmail.com>
2023-05-29 20:32:08 -0300

    first commit

---
+++ numeros.rb
@@ -0,0 +1,12 @@
+# Enteros y float: float contienen al menos 1 digito después del punto(.) Con el correspondiente operador se hacen las operaciones aritmeticas normales. La division entre dos enteros, siempre da un entero. la división entre flotantes da otro resultado: 
+# 10/3 = 3       ||        10/3.0 = 3.333333
+# Para convertir de flotantes a enteros: 10.1257.to_i = 10
+# Para convertir de enteros a flotantes: 10.to_f = 10.0
+# Métodos: 
+  # -10.abs = 10 -> retornarlo a su valor absoluto
+  # 3.even? -> retorna false porque no es par
+  # 2.even? -> retorna true porque es par
+  # 2.next -> 3 siguiente valor.
+  # "2".to_i => 2
+  # "2".to_f => 2.0
+  # 1.to_s => "1"
---
+++ variables.rb
@@ -0,0 +1,12 @@
+# Variable: puede ser string, number, obj, etc. Las variables no definen tipos. El intérprete deduce cuál es el tipo de la variable sin declarar.
+# Constantes: inician con mayusculas, como "Name"
+# ----------------------------- 
+# Palabras reservadas: new, 
+
+
+name = 'Laura'
+# imprime:
+puts name
+# edad = 32
+# tutor = 'Uriel'
+
